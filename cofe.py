da29e6320d48429ba2c71896d8f03ea65cae3e47
Anurag <sarkaranurag104@gmail.com>
2022-04-09 13:23:50 +0530

    inheritence

--- cofe.py
+++ cofe.py
@@ -1,177 +1,123 @@
 import time
-class CoffeeMachine:
+import json
+from pathlib import Path
 
-        def __init__(self,water,milk,coffee):
-            self.__water = water
-            self.__milk = milk
-            self.__coffee = coffee
-            self.__money = 0
-            self.__sale = 0
+class Money:
+    def __init__(self):
+        self.__money = 0
+    
+    def passmoney(self):
+        return self.__money
+    def takemoney(self,givenmoney,cost):
+        i = 0
+        if givenmoney == cost:
+            self.__money += cost
+            return True
+        elif givenmoney < cost:
+            while givenmoney < cost:
+                if i == 3:
+                    print("Nahi Khelna mujhe!!")
+                    print("apne paise leke bhaag ja: ",givenmoney)
+                    return False
 
-        def refil(self):
-            c = input("Default of Custom Refil: ")
-            if c.lower() == "default":
-                self.__water += 500
-                self.__milk += 500
-                self.__coffee += 500
-            elif c.lower() == "custom":
-                self.__water += int(input("Water: "))
-                self.__milk += int(input("Milk: "))
-                self.__coffee += int(input("Coffee: "))
-            else:
-                print("Pardon?")    
-            print("Refil Successfull!")
-                
-
-        def report(self):
-            if self.__water < 0 and self.__milk < 0 and self.__coffee < 0:
-                self.__water = 0
-                self.__milk = 0
-                self.__coffee = 0  
-            return self.__water,self.__milk,self.__coffee,self.__money,self.__sale
+                diff = cost-givenmoney
+                print("Amount Due: ",diff)
+                m = int(input())
+                givenmoney += m
+                i+=1
+        if givenmoney >= cost:
+            if givenmoney == cost:
+                self.__money += cost
+                return True
+            elif givenmoney > cost:
+                diff = givenmoney - cost
+                print("\nHere's Your Change: ",diff)
+                self.__money += cost
+                return True
         
-        def dispense(self,order):
-            if order.lower() == "espresso":
-                if self.__water >= 50 and self.__milk >= 100 and self.__coffee >= 20:
-                    i = 0
-                    money = int(input("It will be Rs.200: "))
-                    if money == 200:
-                        print("Preparing Your order...")
-                        time.sleep(3)
-                        print("Here's Your Espresso... Enjoy!")
-                        print()
-                        self.__water -= 50
-                        self.__milk -= 100
-                        self.__coffee -= 20
-                        self.__money += 200
-                    if money >= 200:
-                        print("Preparing Your order...")
-                        time.sleep(3)
-                        print("Here's Your Espresso... Enjoy!")
-                        if money > 200:
-                            print(f"Your Change Rs.{money-200} ")
-                        self.__water -= 50
-                        self.__milk -= 100
-                        self.__coffee -= 20
-                        self.__money += 200
-                        self.__sale += 1
-                    while money < 200:
-                        if money < 200:
-                            diff = 200-money
-                            print("We need Rs.",diff,"more")
-                            m = int(input())
-                            money += m
-                        if money >= 200:
-                            print("Preparing Your order...")
-                            time.sleep(3)
-                            print("Here's Your Espresso... Enjoy!")
-                            if money > 200:
-                                print(f"Your Change Rs.{money-200} ")
-                            self.__water -= 50
-                            self.__milk -= 100
-                            self.__coffee -= 20
-                            self.__money += 200
-                            self.__sale += 1
-                        if i == 2:
-                            print("Sorry we cant provide coffee to chindi!!!")
-                            break
-                        i+=1
-                else:
-                    print("Sorry, There is not much resource to prepare your order!!!")
 
-            if order.lower() == "cappuccino":
-                if self.__water >= 50 and self.__milk >= 100 and self.__coffee >= 20:
-                    i = 0
-                    money = int(input("It will be Rs.300: "))
-                    if money == 300:
-                        print("Preparing Your order...")
-                        time.sleep(3)
-                        print("Here's Your Cappuccino... Enjoy!")
-                        print()
-                        self.__water -= 50
-                        self.__milk -= 100
-                        self.__coffee -= 20
-                        self.__money += 300
-                    if money >= 300:
-                            print("Preparing Your order...")
-                            time.sleep(3)
-                            print("Here's Your Cappuccion... Enjoy!")
-                            if money > 300:
-                                print(f"Your Change Rs.{money-300} ")
-                            self.__water -= 50
-                            self.__milk -= 100
-                            self.__coffee -= 20
-                            self.__money += 300
-                            self.__sale += 1
+    def ordermoney(self,order,money):
+        if order == "espresso": 
+            return self.takemoney(money,200)
+        if order == "cappuccino":
+            return self.takemoney(money,300)
+        if order == "latte":
+            return self.takemoney(money,150)
 
-                    while money < 300:
-                        if money < 300:
-                            diff = 300-money
-                            print("We need Rs.",diff,"more")
-                            m = int(input())
-                            money += m
-                        if money >= 300:
-                            print("Preparing Your order...")
-                            time.sleep(3)
-                            print("Here's Your Cappuccion... Enjoy!")
-                            if money > 300:
-                                print(f"Your Change Rs.{money - 300} ")
-                            self.__water -= 50
-                            self.__milk -= 100
-                            self.__coffee -= 20
-                            self.__money += 300
-                            self.__sale += 1
-                        if i == 2:
-                            print("Sorry we cant provide coffee to chindi!!!")
-                            break
-                        i+=1
-                else:
-                    print("Sorry, There is not much resource to prepare your order!!!")
-                
-            if order.lower() == "latte":
-                if self.__water >= 50 and self.__milk >= 100 and self.__coffee >= 20:
-                    i = 0
-                    money = int(input("It will be Rs.150: "))
-                    if money == 150:
-                        print("Preparing Your order...")
-                        time.sleep(3)
-                        print("Here's Your Latte... Enjoy!")
-                        print()
-                        self.__water -= 50
-                        self.__milk -= 100
-                        self.__coffee -= 20
-                        self.__money += 150
-                    elif money >= 150:
-                            print("Preparing Your order...")
-                            time.sleep(3)
-                            print("Here's Your Latte... Enjoy!")
-                            if money > 150:
-                                print(f"Your Change Rs.{money-150} ")
-                            self.__water -= 50
-                            self.__milk -= 100
-                            self.__coffee -= 20
-                            self.__money += 150
-                            self.__sale += 1
-                    while money < 150:
-                        if money < 150:
-                            diff = 150-money
-                            print("We need Rs.",diff,"more")
-                            m = int(input())
-                            money += m
-                        if money == 150:
-                            print("Preparing Your order...")
-                            time.sleep(3)
-                            print("Here's Your Latte... Enjoy!")   
-                            if money > 150:
-                                print(f"Your Change Rs.{money-150} ")                         
-                            self.__water -= 50
-                            self.__milk -= 100
-                            self.__coffee -= 20
-                            self.__money += 150
-                            self.__sale += 1
-                        if i == 2:
-                            print("Sorry we cant provide coffee to chindi!!!")
-                            break
-                        i+=1
-                else:
-                    print("Sorry, There is not much resource to prepare your order!!!")
+
+class CoffeeMachine(Money):
+    def __init__(self,water,milk,coffee):
+        super().__init__()
+        self.__water = water
+        self.__milk = milk
+        self.__coffee = coffee
+        self.__sale = 0
+
+    @staticmethod
+    def prep_order(order,coffee,milk,water,money):
+        row = {"order":order,"coffee left":coffee,"milk left":milk,"water left":water,"Money":money}
+        data = json.dumps(row)
+        Path("coffee.json").write_text(data)
+        print("Preparing Your",order.capitalize())  
+        time.sleep(2)
+        print("Here's Your",order.title()) 
+
+
+
+    def refil(self):
+        c = input("Default of Custom Refil: ")
+        if c.lower() == "default":
+            self.__water += 500
+            self.__milk += 500
+            self.__coffee += 500
+            print("Refil Successfull!")
+        elif c.lower() == "custom":
+            self.__water += int(input("Water: "))
+            self.__milk += int(input("Milk: "))
+            self.__coffee += int(input("Coffee: "))
+            print("Refil Successfull!")
+        else:
+            print("Pardon?")    
+            
+
+    def report(self):
+        if self.__water < 0 and self.__milk < 0 and self.__coffee < 0:
+            self.__water = 0
+            self.__milk = 0
+            self.__coffee = 0  
+
+        return self.__water,self.__milk,self.__coffee,self.passmoney(),self.__sale
+
+    def dispense(self,order):
+        if order.lower() == "espresso" and self.__water > 100 and self.__milk > 200 and self.__coffee > 50 :
+            cost = int(input("Please pay Rs.200: "))
+            self.ordermoney(order,cost)  
+            self.__sale += 1
+            self.__coffee -= 50
+            self.__water -= 100
+            self.__milk -= 200 
+            self.prep_order(order,self.__coffee,self.__milk,self.__water,self.passmoney())
+
+        if order.lower() == "cappuccino" and self.__water > 50 and self.__milk > 250 and self.__coffee > 80:
+            cost = int(input("Please pay Rs.300: "))
+            self.ordermoney(order,cost)
+            self.__coffee -= 50
+            self.__water -= 100
+            self.__milk -= 200 
+            self.__sale += 1
+            self.prep_order(order,self.__coffee,self.__milk,self.__water,self.passmoney())
+
+        if order.lower() == "latte" and self.__water > 150 and self.__milk > 150 and self.__coffee > 30:
+            cost = int(input("Please pay Rs.150: "))
+            self.ordermoney(order,cost)
+            self.__coffee -= 50
+            self.__water -= 100
+            self.__milk -= 200 
+            self.__sale += 1
+            self.prep_order(order,self.__coffee,self.__milk,self.__water,self.passmoney())
+
+        elif self.__water < 50 and self.__milk < 150 and self.__coffee < 30:
+            print("Sorry Resources to prepare",order.title(),"has depleted..\nNeeds Refil")
+
+
+
--- coffee.py
+++ coffee.py
@@ -1,4 +1,4 @@
-
+import time
 from cofe import CoffeeMachine
 
 try:
@@ -17,22 +17,20 @@ try:
 
         if choice.lower() == "off":
             print("Powering Off...")
-            time.sleep(2)
+            time.sleep(1)
             break
 
         if choice.lower() == "refil":
             coffee.refil()
 
         if choice.lower() == "report":
-            print("---Todays report---")
+            print("\n---Todays report---")
             r = coffee.report()
             print("Water: ",r[0],"ml",sep="")
             print("Milk: ",r[1],"ml",sep="")
             print("Coffee: ",r[2],"gm",sep="")
             print("Money: Rs.",r[3],sep="")
             print("Sales: ",r[4],sep="")
-            if r[3] > 1000 and r[4] > 2:
-                print("Hello World!")
 
 except ValueError:
     print("Enter details Correctly")
